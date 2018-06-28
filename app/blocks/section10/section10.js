930a771e22e6ae62c8bc13cb2c0ee629fd841a3c
artem8086 <artemsvyatoha@gmai.com>
2018-06-28 19:58:47 +0300

    work in progress

---
+++ app/blocks/section10/section10.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('.sec10-slider').slick({
+		dots: true,
+		dotsClass: 'slider__paging_vertical',
+		arrows: false,
+		appendDots: $('.sec10-dots'),
+		customPaging(slick, index) {
+			return $('<a class="sec10-slider__button" tabindex="' + index + '">');
+		}
+	});
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,3 +2,4 @@ import '../blocks/slider/slider';
 import '../blocks/section5/section5';
 import '../blocks/section7/section7';
 import '../blocks/section8/section8';
+import '../blocks/section10/section10';
