a8d0616d50bd922af5fb76b8fdea66d48998e38e
artem8086 <artemsvyatoha@gmai.com>
2018-06-28 20:08:14 +0300

    work in progress

--- app/blocks/section10/section10.js
+++ app/blocks/section10/section10.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 		dots: true,
 		dotsClass: 'slider__paging_vertical',
 		arrows: false,
+		vertical: true,
+		verticalSwiping: true,
 		appendDots: $('.sec10-dots'),
 		customPaging(slick, index) {
 			return $('<a class="sec10-slider__button" tabindex="' + index + '">');
