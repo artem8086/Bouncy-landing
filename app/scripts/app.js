9d1d65e7edb0f6c709d91a8e775127f5ad2fa11d
artem8086 <artemsvyatoha@gmai.com>
2018-06-30 12:21:10 +0300

    work in progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,3 +3,24 @@ import '../blocks/section5/section5';
 import '../blocks/section7/section7';
 import '../blocks/section8/section8';
 import '../blocks/section10/section10';
+
+$(document).ready(function () {
+	function smoothScroll(event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+	}
+
+	const menuLink = '.main-nav__link';
+	const activeClass = 'main-nav__link_active';
+
+	$(menuLink).on('click', function (event) {
+		console.log(this);
+		$(menuLink).removeClass(activeClass);
+		$(this).addClass(activeClass);
+		smoothScroll.call(this, event);
+	});
+
+	$('.round-button').on('click', smoothScroll);
+});
